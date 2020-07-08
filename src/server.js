72a88f7ad334d8ac890adec0e755d9010dd7448c
dmmaia <david_mmaia@hotmail.com>
2020-07-08 17:09:00 -0300

    starting using socket-io

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require("path");
 const mongoose = require('mongoose');
+const socketIo = require('socket.io');
 
 const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+io.on('connection', socket => {
+	console.log('New user connected');
+	socket.on('disconnect', () => {
+		console.log('User disconnected')
+	})
+})
 
 mongoose.connect(
 	process.env.MONGO_URL, {
